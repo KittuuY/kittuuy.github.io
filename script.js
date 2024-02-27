758bea501d085177f08a8585efb011cc3995938d
KittuuY <159100901+KittuuY@users.noreply.github.com>
2024-02-27 12:30:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
+a = 0;
 function show() {
- document.getElementById("text")
-  .innerHTML  = "searching...";
+switch (a){
+
+case 0:
+  document.getElementById("text").innerHTML = "Loading..."
+  a = 1;
+  break;
+
+  case 1:
+    document.getElementById("text").innerHTML = "Almost there!"
+    a = 2
+
+    break;
+  case 2:
+    document.getElementById("text").innerHTML = "Web page has been loaded!"
+    a = 3
+    
+    break;
+  case 3:
+    document.getElementById("text").innerHTML = ""
+    a = 0
+    break;
 }
+/*if (a){
+  document.getElementById("text")
+  .innerHTML  = "searching...";
+  a = false;
+}else{
+  document.getElementById("text").innerHTML = ""
+  a = true;
+}*/
+}
+let name = prompt("What is you cat's name?")
+let words = 0;
+if (name == 'cutie pie'){
+    words = ("Welcome, "+name+ "!")
+   }
+else{
+    words = ("Nice to meet you "+name)
+    }
+document.getElementById("hello").innerHTML = words;
